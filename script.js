8e0dc0d3599b0569134b7d37da4c09e82e88a39e
oscarostby <118980639+oscarostby@users.noreply.github.com>
2023-11-21 22:55:32 +0100

    Fikksa snow effect og footer. + bedre luker design

--- script.js
+++ script.js
@@ -12,4 +12,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   
+  function createSnowflake() {
+    const snowflake = document.createElement('div');
+    snowflake.classList.add('snowflake');
+    snowflake.style.left = Math.random() * 100 + 'vw';
+    snowflake.style.opacity = Math.random();
+    snowflake.style.width = Math.random() * 5 + 'px';
+    snowflake.style.height = snowflake.style.width;
+    snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
+    snowflake.style.animationDelay = Math.random() * 5 + 's';
+    document.getElementById('snowflakes-container').appendChild(snowflake);
+  
+    // Remove snowflake after it falls
+    setTimeout(() => {
+      snowflake.remove();
+    }, (parseFloat(snowflake.style.animationDuration) + parseFloat(snowflake.style.animationDelay)) * 1000);
+  }
+  
+  setInterval(createSnowflake, 50); // Adjust the interval to add more/less snowflakes
   
