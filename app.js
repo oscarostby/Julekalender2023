5f29330fe0f3406c4b3d4b99d5ef8371e1dfa6cc
1Sig <Sigurdowre@gmail.com>
2023-11-22 10:07:32 +0100

    updated the app.js with 404 and fixed placement of some files in jul

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const app= require('express')()
+     // static file serve
+     app.use(express.static())
+     // not found in static files, so default to index.html
+     app.use((req, res) => res.sendFile(`$/index.html`))
+app.listen(3000)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// 404 error handler
+app.use((req, res, next) => {
+  res.status(404).sendFile('$/404.html');
+});
